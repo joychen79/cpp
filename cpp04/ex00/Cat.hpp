8737283b0111de52b34cca6c88f45c6a1dcadd2c
Jingjing Chen <jingchen@c1r8s5.42madrid.com>
2024-09-24 18:41:08 +0200

    04

---
+++ cpp04/ex00/Cat.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Cat.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jingchen <jingchen@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/21 19:42:46 by jingchen          #+#    #+#             */
+/*   Updated: 2024/09/24 18:05:21 by jingchen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CAT_HPP
+#define CAT_HPP
+
+#include "Animal.hpp"
+
+class Cat : public Animal
+{
+    public:
+        Cat();
+        Cat(const Cat &copy);
+        Cat &operator=(const Cat &assigned);
+        ~Cat();
+
+        void makeSound() const;
+};
+
+#endif
